3268bfe3b0659373eb1af7160894c5f5fc074667
rainamaharjan <rainapicta@gmail.com>
2022-11-07 16:07:08 +0545

    updated db to version 4

--- packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/InsertNotificationDBHelper.java
+++ packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/InsertNotificationDBHelper.java
@@ -21,7 +21,7 @@ public class InsertNotificationDBHelper extends SQLiteOpenHelper {
 
   }
 
-  private static int DATABASE_VERSION = 3;
+  private static int DATABASE_VERSION = 4;
   private static String DATABASE_NAME = "wlink.db";
   String TABLE_NAME = "NotificationDb";
 
